1ca6ce42de0d26e8e30ad262a1b4bf18478b3920
clovermini22 <clovermini22@gmail.com>
2022-11-03 00:50:54 +0900

    Add CDP input form

--- frontend/src/components/Redeem.vue
+++ frontend/src/components/Redeem.vue
@@ -1,6 +1,22 @@
 <template>
   <div class="uk-width-1-1">
     <hr />
+    <div class="uk-inline">
+      <a class="uk-form-icon uk-form-icon-flip input-form-icon" href="#"
+        ><span>CDP#</span></a
+      >
+      <input
+        class="uk-input input-form uk-form-width-medium uk-form-large"
+        type="number"
+      />
+    </div>
+    <div class="wrap">
+      <span
+        class="icon-circle"
+        uk-icon="icon: arrow-down; ratio: 2;"
+        style="margin-left: -1px"
+      ></span>
+    </div>
     <div class="uk-inline form-icon">
       <a class="uk-form-icon uk-form-icon-flip input-form-icon" href="#"
         ><img src="../img/fox-icon.png" style="width: 20px" /><span>FOX</span>
